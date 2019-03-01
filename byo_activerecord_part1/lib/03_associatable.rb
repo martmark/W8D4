65ef771eac00e2baa3b6a18302cefc00fa499430
martmark <martinmarkaj@live.com>
2019-03-01 18:31:24 -0500

    Add byo_activerecord day2

--- byo_activerecord_part1/lib/03_associatable.rb
+++ byo_activerecord_part1/lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -10,41 +11,71 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+
+    self.model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    options[:foreign_key] = (name.to_s + "_id").to_sym if options[:foreign_key].nil?
+    options[:class_name] = name.to_s.camelcase if options[:class_name].nil?
+    options[:primary_key] = :id if options[:primary_key].nil?
+   
+    options.each do |k,v|
+      self.send("#{k}=", v)
+    end
   end
 end
 
+      
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    options[:foreign_key] = (self_class_name.to_s.downcase + "_id").to_sym if options[:foreign_key].nil?
+    options[:class_name] = name.to_s.singularize.camelcase if options[:class_name].nil?
+    options[:primary_key] = :id if options[:primary_key].nil?
+      
+    options.each do |k,v|
+      self.send("#{k}=", v)
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+  
+
+    options = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = options
+
+    define_method(name) do
+      target = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => target).first
+    end
+ 
   end
 
   def has_many(name, options = {})
-    # ...
+   
+    options = HasManyOptions.new(name, self, options)
+    self.assoc_options[name] = options
+    define_method(name) do
+      target = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => target)
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
--- byo_activerecord_part1/lib/04_associatable2.rb
+++ byo_activerecord_part1/lib/04_associatable2.rb
@@ -5,6 +5,37 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    through_options = self.assoc_options[through_name]
+
+    fk = self.send(through_options.foreign_key)
+    define_method(name) do
+      #debugger
+    source_options =
+      through_options.model_class.assoc_options[source_name]
+      pk = self.send(source_options.primary_key)
+
+
+    DBConnection.execute(<<-SQL, )
+      
+    SQL
+
+      
+      
+    end
+
+    # source_options = self.name
+    # debugger
+    # source_options.each do |option|
+    #   self.parse_all(options)
+    # end
   end
+
+
 end
+
+
+# DBConnection.execute(<<-SQL)
+#       SELECT *
+#       FROM #{ self.table_name }
+#       LIMIT 0
+#     SQL
